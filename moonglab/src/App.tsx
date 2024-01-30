763fc49431d631dcfcb5841cf7a9b220f347ddea
JustMoong <andudyun0504@gmail.com>
2024-01-30 17:11:51 +0900

    react

---
+++ moonglab/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Moonglab</h1>
+      <div className="App-header">
+        <span></span>
+        </div>
+    </div>
+  );
+}
+
+export default App;

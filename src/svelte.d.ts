ff08075d22971599c11a6fcbb35070b86df612cd
윤무영 <yun@MacBook-Air-M2.local>
2024-10-03 19:20:25 +0900

    clear

--- src/svelte.d.ts
+++
@@ -1,6 +0,0 @@
-// svelte.d.ts
-declare module "*.svelte" {
-    import { SvelteComponentTyped } from "svelte";
-    class ComponentName extends SvelteComponentTyped<{propertyName: string;}> {}
-    export default value;
-  }

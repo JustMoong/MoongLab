b0397b879af307c6452741b00aa133f8c63504eb
Justmoong <andudyun0504@gmail.com>
2024-10-08 16:36:38 +0900

    fix package dir issue

---
+++ moonglab/src/components/UIHeader.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let title = "Visit Apple";
+    export let description = "Click the link above to visit Apple.com for more information.";
+</script>
+
+<style>
+    .header {
+        text-align: center;
+        margin: 20px;
+    }
+    .link {
+        font-size: 24px;
+        color: blue;
+        text-decoration: none;
+    }
+    .description {
+        margin-top: 10px;
+        font-size: 16px;
+        color: gray;
+    }
+</style>
+
+<div class="header">
+    <a class="link" href="https://www.apple.com" target="_blank" rel="noopener noreferrer">{title}</a>
+    <p class="description">{description}</p>
+</div>
